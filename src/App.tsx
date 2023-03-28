d539dc9d54b6c9ca292497a614b8aebce730338f
yanky <o.anikin@chillicode.ru>
2023-03-29 00:49:15 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <DefaultLayout>
-          <form onSubmit={(e)=>e.preventDefault()}>
+          <form onSubmit={(e)=>alert(1)}>
           <label htmlFor="expire-date-month">Expiration Date:</label>
           <input
               type="text"
